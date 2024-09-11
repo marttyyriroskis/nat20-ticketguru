3773b5ffa885b720748f763925bd7a34ef66e3af
marttyyriroskis <julia.hamalainen@myy.haaga-helia.fi>
2024-09-11 15:59:01 +0300

    Create empty springboot project

---
+++ nat20xticketguru/src/main/java/ohjelmistoprojekti1/nat20xticketguru/Nat20xticketguruApplication.java
@@ -0,0 +1,13 @@
+package ohjelmistoprojekti1.nat20xticketguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nat20xticketguruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nat20xticketguruApplication.class, args);
+	}
+
+}
---
+++ nat20xticketguru/src/test/java/ohjelmistoprojekti1/nat20xticketguru/Nat20xticketguruApplicationTests.java
@@ -0,0 +1,13 @@
+package ohjelmistoprojekti1.nat20xticketguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nat20xticketguruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
