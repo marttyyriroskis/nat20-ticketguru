5f3d031ae368a54e8b659bc0c4b0fea169a3c7d9
Phoolis <carlsonps@gmail.com>
2024-09-22 12:03:56 +0300

    Add User, and Role, entities

---
+++ src/main/java/com/nat20/ticketguru/domain/Role.java
@@ -0,0 +1,67 @@
+package com.nat20.ticketguru.domain;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "role")
+    @JsonIgnore
+    private List<User> users;
+
+    public Role() {
+    }
+
+    public Role(String title) {
+        this.title = title;
+    }
+
+    public Role(String title, List<User> users) {
+        this.title = title;
+        this.users = users;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Role [id=" + id + ", title=" + title + "]";
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+}
