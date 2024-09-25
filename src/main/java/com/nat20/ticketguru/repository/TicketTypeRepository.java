eeefdf7fb1bee537989261ab9a0b94225a5d21ab
marttyyriroskis <julia.hamalainen@myy.haaga-helia.fi>
2024-09-25 11:42:42 +0300

    Add Repository package

--- src/main/java/com/nat20/ticketguru/repository/TicketTypeRepository.java
+++ src/main/java/com/nat20/ticketguru/repository/TicketTypeRepository.java
@@ -1,6 +1,8 @@
-package com.nat20.ticketguru.domain;
+package com.nat20.ticketguru.repository;
 
 import org.springframework.data.repository.CrudRepository;
+
+import com.nat20.ticketguru.domain.*;
 
 public interface TicketTypeRepository extends CrudRepository<TicketType, Long> {
 
