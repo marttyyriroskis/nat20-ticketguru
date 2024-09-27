df7b44e694007786cb2d63fc799c8a3d87b6883b
marttyyriroskis <julia.hamalainen@myy.haaga-helia.fi>
2024-09-27 16:57:39 +0300

    Add GET Events to RestEventController

--- src/main/java/com/nat20/ticketguru/api/RestEventController.java
+++ src/main/java/com/nat20/ticketguru/api/RestEventController.java
@@ -3,14 +3,23 @@ package com.nat20.ticketguru.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nat20.ticketguru.domain.Event;
 import com.nat20.ticketguru.repository.EventRepository;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 @RequestMapping("/api")
 public class RestEventController {
     @Autowired
     EventRepository eventRepository;
+
+    // Get events
+    @GetMapping("/events")
+    public Iterable<Event> getEvents() {
+        return eventRepository.findAll();
+    }
 
 }
