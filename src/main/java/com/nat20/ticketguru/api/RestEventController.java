192ca4d6ae4c86adcc7cf55beab40f93c15afd14
Phoolis <carlsonps@gmail.com>
2024-09-28 16:21:45 +0300

    Add stashed changes to PUT request

--- src/main/java/com/nat20/ticketguru/api/RestEventController.java
+++ src/main/java/com/nat20/ticketguru/api/RestEventController.java
@@ -1,6 +1,11 @@
 package com.nat20.ticketguru.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nat20.ticketguru.domain.Event;
@@ -8,11 +13,6 @@ import com.nat20.ticketguru.repository.EventRepository;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-
 
 @RestController
 @RequestMapping("/api")
@@ -26,17 +26,23 @@ public class RestEventController {
     public Iterable<Event> getEvents() {
         return eventRepository.findAll();
     }
+
     // Get event by id
     @GetMapping("/event/{id}")
-    <Optional>Event getEvent(@PathVariable("id") Long eventId) {
+    <Optional> Event getEvent(@PathVariable("id") Long eventId) {
         return eventRepository.findById(eventId).orElse(null);
     }
+
     // Post a new event
     @PostMapping("/events")
     public Event createEvent(@RequestBody Event event) {
         Event savedEvent = eventRepository.save(event);
         return savedEvent;
     }
-    
 
+    @PutMapping("events/{id}")
+    public Event editEvent(@RequestBody Event editedEvent, @PathVariable("id") Long eventId) {
+        editedEvent.setId(eventId);
+        return eventRepository.save(editedEvent);
+    }
 }
