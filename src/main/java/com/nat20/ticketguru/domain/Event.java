8566d2bc2ac869507a20cfec2c2b50f966f97df0
marttyyriroskis <julia.hamalainen@myy.haaga-helia.fi>
2024-09-29 12:42:14 +0300

    Remove JsonIgnore import from Event class

--- src/main/java/com/nat20/ticketguru/domain/Event.java
+++ src/main/java/com/nat20/ticketguru/domain/Event.java
@@ -2,8 +2,6 @@ package com.nat20.ticketguru.domain;
 
 import java.time.LocalDateTime;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
