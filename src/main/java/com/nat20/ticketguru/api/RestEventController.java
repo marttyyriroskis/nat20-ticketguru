2b9fdcb027340d196d0417e7c62b567fc8dacdfc
Phoolis <carlsonps@gmail.com>
2024-09-30 09:00:57 +0300

    Fix Event api base_url to /api/events/

--- src/main/java/com/nat20/ticketguru/api/RestEventController.java
+++ src/main/java/com/nat20/ticketguru/api/RestEventController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -28,7 +27,7 @@ import com.nat20.ticketguru.repository.VenueRepository;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/events/")
 @Validated
 public class RestEventController {
 
@@ -39,13 +38,13 @@ public class RestEventController {
     VenueRepository venueRepository;
 
     // Get events
-    @GetMapping("/events")
+    @GetMapping("")
     public Iterable<Event> getEvents() {
         return eventRepository.findAll();
     }
 
     // Get event by id
-    @GetMapping("/events/{id}")
+    @GetMapping("{id}")
     public Event getEvent(@PathVariable("id") Long eventId) {
         Optional<Event> optionalEvent = eventRepository.findById(eventId);
         if (!optionalEvent.isPresent()) {
@@ -57,9 +56,9 @@ public class RestEventController {
     }
 
     // Post a new event
-    @PostMapping("/events")
+    @PostMapping("")
     public Event createEvent(@RequestBody Event event) {
-        
+
         // checks if the venue id is null instead of entire venue being null
         if (event.getVenue() != null && event.getVenue().getId() == null) {
             event.setVenue(null);
@@ -81,7 +80,7 @@ public class RestEventController {
     }
 
     // Edit event with PUT request
-    @PutMapping("events/{id}")
+    @PutMapping("{id}")
     public Event editEvent(@Valid @RequestBody Event editedEvent, @PathVariable("id") Long eventId) {
         Optional<Event> optionalEvent = eventRepository.findById(eventId);
         if (!optionalEvent.isPresent()) {
