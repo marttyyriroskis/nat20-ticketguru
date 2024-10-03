74a29b708cff89cd01a8cba12d5d56654261a1ec
marttyyriroskis <julia.hamalainen@myy.haaga-helia.fi>
2024-10-03 09:34:39 +0300

    Remove TicketSale connections from TicketguruApplication + remove TicketSaleRepository

--- src/main/java/com/nat20/ticketguru/repository/TicketSaleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nat20.ticketguru.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.nat20.ticketguru.domain.TicketSale;
-
-public interface TicketSaleRepository extends CrudRepository<TicketSale, Long> {
-
-}
