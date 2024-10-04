7c30cf2f12c93adee47e24f409ec4b9ba6c9dcd5
devaajanne <devaajanne@gmail.com>
2024-10-04 12:19:06 +0300

    Add GET requests to ticket type controller

--- src/main/java/com/nat20/ticketguru/api/RestTicketTypeController.java
+++ src/main/java/com/nat20/ticketguru/api/RestTicketTypeController.java
@@ -1,13 +1,18 @@
 package com.nat20.ticketguru.api;
 
+import java.util.Optional;
+
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nat20.ticketguru.domain.TicketType;
 import com.nat20.ticketguru.repository.TicketTypeRepository;
 
 @RestController
-@RequestMapping("/api/tickets")
+@RequestMapping("/api/tickettypes")
 @Validated
 public class RestTicketTypeController {
 
@@ -18,8 +23,16 @@ public class RestTicketTypeController {
     }
 
     // Get all ticket types
+    @GetMapping
+    public Iterable<TicketType> getTicketTypes() {
+        return ticketTypeRepository.findAll();
+    }
 
     // Get ticket type by id
+    @GetMapping("{id}")
+    public Optional<TicketType> getTicketType(@PathVariable("id") Long id) {
+        return ticketTypeRepository.findById(id);
+    }
 
     // Add a new ticket type
 
