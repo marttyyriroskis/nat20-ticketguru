04267fe9f735c09a1f81e72b3506c017c9fdc28a
marttyyriroskis <julia.hamalainen@myy.haaga-helia.fi>
2024-10-05 11:07:50 +0300

    Start building RestTicketController

--- src/main/java/com/nat20/ticketguru/api/RestTicketController.java
+++ src/main/java/com/nat20/ticketguru/api/RestTicketController.java
@@ -2,7 +2,7 @@ package com.nat20.ticketguru.api;
 
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,21 +12,26 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.nat20.ticketguru.domain.Ticket;
 import com.nat20.ticketguru.repository.TicketRepository;
 import com.nat20.ticketguru.domain.TicketType;
 import com.nat20.ticketguru.repository.TicketTypeRepository;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping("/api/tickets")
 @Validated
 public class RestTicketController {
-    @Autowired
-    TicketRepository ticketRepository;
+    private final TicketRepository ticketRepository;
+    private final TicketTypeRepository ticketTypeRepository;
 
-    @Autowired
-    TicketTypeRepository ticketTypeRepository;
+    public RestTicketController(TicketRepository ticketRepository, TicketTypeRepository ticketTypeRepository) {
+        this.ticketRepository = ticketRepository;
+        this.ticketTypeRepository = ticketTypeRepository;
+    }
 
     // Get tickets
     @GetMapping("")
@@ -35,26 +40,50 @@ public class RestTicketController {
     }
     
     // Get ticket by id
-    @GetMapping("{id}")
-    public Optional<Ticket> getTicket(@PathVariable("id") Long asiakasId) {
-        return ticketRepository.findById(asiakasId);
+    @GetMapping("/{id}")
+    public Ticket getTicket(@PathVariable("id") Long ticketId) {
+        Optional<Ticket> optionalTicket = ticketRepository.findById(ticketId);
+        if (!optionalTicket.isPresent()) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Ticket not found");
+        }
+        Ticket ticket = optionalTicket.get();
+        return ticket;
     }
     
     // Post a new ticket
     @PostMapping("")
-    public Ticket newTicket(@RequestBody Ticket newTicket) {
-        return ticketRepository.save(newTicket);
+    public Ticket createTicket(@Valid @RequestBody Ticket ticket) {
+        // Check if TicketType.id is null
+        if (ticket.getTicketType() != null && ticket.getTicketType().getId() == null) {
+            ticket.setTicketType(null);
+        }
+
+        // Check if TicketType already exists
+        if (ticket.getTicketType() != null) {
+            Optional<TicketType> existingTicketType = ticketTypeRepository.findById(ticket.getTicketType().getId());
+
+            if (!existingTicketType.isPresent()) {
+                throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "Ticket type does not exist!");
+            }
+
+            ticket.setTicketType(existingTicketType.get());
+        }
+
+        Ticket savedTicket = ticketRepository.save(ticket);
+        return savedTicket;
     }
     
     // Edit ticket
-    @PutMapping("{id}")
+    @PutMapping("/{id}")
     public Ticket editTicket(@RequestBody Ticket editedTicket, @PathVariable Long id) {
         editedTicket.setId(id);
         return ticketRepository.save(editedTicket);
     }
 
     // Delete ticket
-    @DeleteMapping("{id}")
+    @DeleteMapping("/{id}")
     public Iterable<Ticket> deleteTicket(@PathVariable Long id) {
         ticketRepository.deleteById(id);
         return ticketRepository.findAll();
