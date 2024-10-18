a736a60495b406fd65f126db2b9a34e85b21854e
marttyyriroskis <julia.hamalainen@myy.haaga-helia.fi>
2024-10-18 11:45:22 +0300

    Add DTO and ResponseEntity logic to RestTicketController

--- src/main/java/com/nat20/ticketguru/dto/TicketDto.java
+++ src/main/java/com/nat20/ticketguru/dto/TicketDto.java
@@ -7,11 +7,11 @@ import jakarta.validation.constraints.NotNull;
 public record TicketDTO (
     @NotNull(message = "Barcode must not be null") String barcode,
 
-    @NotNull(message = "Used At must not be null") LocalDateTime usedAt,
+    LocalDateTime usedAt,
 
     @NotNull(message = "Price must not be null") double price,
 
     @NotNull(message = "Ticket ID must not be null") Long ticketTypeId,
 
-    @NotNull(message = "Sale ID must not be null") Long saleId
+    Long saleId
 ) {}
