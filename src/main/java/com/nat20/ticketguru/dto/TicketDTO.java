17ad76172f79f01083a0af420e67a525c131c469
Phoolis <carlsonps@gmail.com>
2024-10-18 16:39:37 +0300

    Fix TicketDTO space in name

--- src/main/java/com/nat20/ticketguru/dto/TicketDTO.java
+++ src/main/java/com/nat20/ticketguru/dto/TicketDTO.java
@@ -4,14 +4,14 @@ import java.time.LocalDateTime;
 
 import jakarta.validation.constraints.NotNull;
 
-public record TicketDTO (
-    @NotNull(message = "Barcode must not be null") String barcode,
+public record TicketDTO(
+        @NotNull(message = "Barcode must not be null")
+        String barcode,
+        LocalDateTime usedAt,
+        @NotNull(message = "Price must not be null")
+        double price,
+        @NotNull(message = "Ticket ID must not be null")
+        Long ticketTypeId,
+        Long saleId) {
 
-    LocalDateTime usedAt,
-
-    @NotNull(message = "Price must not be null") double price,
-
-    @NotNull(message = "Ticket ID must not be null") Long ticketTypeId,
-
-    Long saleId
-) {}
+}
