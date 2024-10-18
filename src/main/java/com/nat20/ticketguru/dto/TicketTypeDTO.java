2725f715fb3c5a66f8edc33f91aa27bffe83844d
devaajanne <devaajanne@gmail.com>
2024-10-18 14:43:15 +0300

    Add missing attribute to TicketTypeDTO

--- src/main/java/com/nat20/ticketguru/dto/TicketTypeDTO.java
+++ src/main/java/com/nat20/ticketguru/dto/TicketTypeDTO.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 public record TicketTypeDTO(
+        long id,
         @Size(min = 1, max = 100, message = "Name must be between 1 and 100 characters long") String name,
         @Positive(message = "Price must be positive") double retail_price,
         @Positive(message = "Total available must be positive or null") Integer total_available,
