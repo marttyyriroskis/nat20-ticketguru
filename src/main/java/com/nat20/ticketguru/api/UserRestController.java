6654071a9aa8e7db88135413f671f28df090ff81
Hellman Studios <jesse.hellman@hellmanstudios.fi>
2024-10-21 16:31:05 +0300

    Add validated annotation to user related controllers

--- src/main/java/com/nat20/ticketguru/api/UserRestController.java
+++ src/main/java/com/nat20/ticketguru/api/UserRestController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,6 +36,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/api/users")
+@Validated
 public class UserRestController {
 
     @Autowired
