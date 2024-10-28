bfb5db06d3e52948a4c75e6418ea7e699dfd8595
devaajanne <devaajanne@gmail.com>
2024-10-28 09:41:07 +0200

    Add events and ticket types to permissions list

--- src/main/java/com/nat20/ticketguru/domain/Permission.java
+++ src/main/java/com/nat20/ticketguru/domain/Permission.java
@@ -1,9 +1,23 @@
 package com.nat20.ticketguru.domain;
 
 public enum Permission {
+    // Sales permissions
     VIEW_SALES,
     EDIT_SALES,
     CREATE_SALES,
     DELETE_SALES,
+
+    // Events permissions
+    VIEW_EVENTS,
+    EDIT_EVENTS,
+    CREATE_EVENTS,
+    DELETE_EVENTS,
+
+    // Ticket type permissions
+    VIEW_TICKET_TYPES,
+    EDIT_TICKET_TYPES,
+    CREATE_TICKET_TYPES,
+    DELETE_TICKET_TYPES,
+    
     // add others as needed
 }
