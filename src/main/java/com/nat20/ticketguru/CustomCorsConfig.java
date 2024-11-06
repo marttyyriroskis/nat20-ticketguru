8d7837b4500407bc7a4ec338f34328ac79cec14e
Phoolis <carlsonps@gmail.com>
2024-11-06 22:30:18 +0200

    Modify CORS to accept front-end test origin http://localhost:5173 and all api/tickets endpoints

--- src/main/java/com/nat20/ticketguru/CustomCorsConfig.java
+++ src/main/java/com/nat20/ticketguru/CustomCorsConfig.java
@@ -14,14 +14,14 @@ public class CustomCorsConfig implements CorsConfigurationSource {
 
     @Override
     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-        
+
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:8080", "http://127.0.0.1:8080")); // TODO: Specify allowed origin(s)
+        config.setAllowedOrigins(List.of("http://localhost:8080", "http://localhost:5173")); // TODO: Specify allowed origin(s)
         config.setAllowedMethods(List.of("GET", "PUT")); // Allow specified HTTP methods
         config.setAllowedHeaders(List.of("*")); // Allow all headers
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	    source.registerCorsConfiguration("api/tickets", config); // Specify endpoint(s)
+        source.registerCorsConfiguration("/api/tickets*", config); // Specify endpoint(s)
 
         return config;
 
