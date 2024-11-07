a76dd4e381bfc426bfbb99912ad19671cf1367f2
Phoolis <carlsonps@gmail.com>
2024-11-07 20:56:07 +0200

    Poke CORS a bit more: now both GET and PUT requests work cross-origin as intended

--- src/main/java/com/nat20/ticketguru/CustomCorsConfig.java
+++ src/main/java/com/nat20/ticketguru/CustomCorsConfig.java
@@ -2,28 +2,29 @@ package com.nat20.ticketguru;
 
 import java.util.List;
 
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import jakarta.servlet.http.HttpServletRequest;
+@Configuration
+public class CustomCorsConfig {
 
-@Component
-public class CustomCorsConfig implements CorsConfigurationSource {
-
-    @Override
-    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+    @Bean
+    public CorsConfigurationSource getCorsConfigurationSource() {
 
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:8080", "http://localhost:5173")); // TODO: Specify allowed origin(s)
-        config.setAllowedMethods(List.of("GET", "PUT")); // Allow specified HTTP methods
+        config.setAllowedOrigins(List.of("http://localhost:5173")); // TODO: Specify allowed origin(s)
+        config.setAllowedMethods(List.of("GET", "PUT", "OPTIONS")); // Allow specified HTTP methods
         config.setAllowedHeaders(List.of("*")); // Allow all headers
+        config.setAllowCredentials(true);
+        config.setExposedHeaders(List.of("Authorization", "Content-Type"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/api/tickets*", config); // Specify endpoint(s)
+        source.registerCorsConfiguration("/api/tickets/**", config); // Specify endpoint(s)
 
-        return config;
+        return source;
 
     }
 
--- src/main/java/com/nat20/ticketguru/WebSecurityConfig.java
+++ src/main/java/com/nat20/ticketguru/WebSecurityConfig.java
@@ -37,12 +37,14 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(
-                authorize -> authorize
-                        .requestMatchers(antMatcher("/css/**")).permitAll()
-                        .requestMatchers(WHITE_LIST_URLS).permitAll()
-                        .anyRequest().authenticated()
-                    )
+                .cors(cors -> cors.configurationSource(customCorsConfig.getCorsConfigurationSource()))
+                .csrf(csrf -> csrf.disable()) // TODO: disable csrf for now, enable later for production?
+                .authorizeHttpRequests(
+                        authorize -> authorize
+                                .requestMatchers(antMatcher("/css/**")).permitAll()
+                                .requestMatchers(WHITE_LIST_URLS).permitAll()
+                                .anyRequest().authenticated()
+                )
                 .httpBasic(Customizer.withDefaults())
                 .headers(
                         headers -> headers
@@ -55,9 +57,7 @@ public class WebSecurityConfig {
                                 .defaultSuccessUrl("/index", true)
                                 .permitAll()
                 )
-                .logout(logout -> logout.permitAll())
-                .csrf(csrf -> csrf.disable()) // TODO: disable csrf for now, enable later for production?
-                .cors(cors -> cors.configurationSource(customCorsConfig));
+                .logout(logout -> logout.permitAll());
 
         return http.build();
 
