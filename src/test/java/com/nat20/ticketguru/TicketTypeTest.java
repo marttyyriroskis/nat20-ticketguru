52ea3622b218af157321787b9d1f71f48600f126
devaajanne <devaajanne@gmail.com>
2024-11-20 08:11:24 +0200

    Refactored and Added @BeforeEach to TicketTypeTest

--- src/test/java/com/nat20/ticketguru/TicketTypeTest.java
+++ src/test/java/com/nat20/ticketguru/TicketTypeTest.java
@@ -5,6 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -25,72 +27,59 @@ import jakarta.transaction.Transactional;
 @SpringBootTest(properties = "spring.profiles.active=test")
 public class TicketTypeTest {
 
-    @Autowired
-    private TicketTypeRepository ticketTypeRepository;
+        @Autowired
+        private TicketTypeRepository ticketTypeRepository;
 
-    @Autowired
-    private EventRepository eventRepository;
+        @Autowired
+        private EventRepository eventRepository;
 
-    @Autowired
-    private VenueRepository venueRepository;
+        @Autowired
+        private VenueRepository venueRepository;
 
-    @Autowired
-    private ZipcodeRepository zipcodeRepository;
+        @Autowired
+        private ZipcodeRepository zipcodeRepository;
 
-    @Test
-    public void testTicketTypeSoftDelete() {
-        // Set up
-        Zipcode zipcode = zipcodeRepository.save(new Zipcode("00100", "Helsinki"));
-        Venue venue = venueRepository.save(
-                new Venue("Bunkkeri", "Bunkkeritie 1", zipcodeRepository.findByZipcode(zipcode.getZipcode()), null));
-        Event event = eventRepository.save(new Event("Death metal karaoke", "Öriöriöriöriörirprir!!!!!", 10,
-                LocalDateTime.of(2055, 10, 12, 12, 00), LocalDateTime.of(2055, 10, 12, 12, 00),
-                LocalDateTime.of(2055, 10, 12, 12, 00), venueRepository.findById(venue.getId()).get(), null));
+        private TicketType ticketType;
 
-        TicketType ticketType = ticketTypeRepository
-                .save(new TicketType("adult", 29.99, null, eventRepository.findById(event.getId()).get(), null));
+        @BeforeEach
+        public void setup() {
+                Zipcode zipcode = zipcodeRepository.save(new Zipcode("00100", "Helsinki"));
+                Venue venue = venueRepository.save(new Venue("Bunkkeri", "Bunkkeritie 1", zipcode, null));
+                Event event = eventRepository.save(new Event(
+                                "Death metal karaoke",
+                                "Öriöriöriöriörirprir!!!!!",
+                                10,
+                                LocalDateTime.of(2055, 10, 12, 12, 0),
+                                LocalDateTime.of(2055, 10, 12, 12, 0),
+                                LocalDateTime.of(2055, 10, 12, 12, 0),
+                                venue,
+                                null));
 
-        ticketType.delete();
+                ticketType = ticketTypeRepository.save(new TicketType("adult", 29.99, null, event, null));
+        }
 
-        assertTrue(ticketType.getDeletedAt() instanceof LocalDateTime, "delete() method failed, deletedAt should be of type LocalDateTime");
-    }
+        @Test
+        public void testTicketTypeSoftDelete() {
+                ticketType.delete();
 
-    @Test
-    public void testTicketTypeRestore() {
-        // Set up
-        Zipcode zipcode = zipcodeRepository.save(new Zipcode("00100", "Helsinki"));
-        Venue venue = venueRepository.save(
-                new Venue("Bunkkeri", "Bunkkeritie 1", zipcodeRepository.findByZipcode(zipcode.getZipcode()), null));
-        Event event = eventRepository.save(new Event("Death metal karaoke", "Öriöriöriöriörirprir!!!!!", 10,
-                LocalDateTime.of(2055, 10, 12, 12, 00), LocalDateTime.of(2055, 10, 12, 12, 00),
-                LocalDateTime.of(2055, 10, 12, 12, 00), venueRepository.findById(venue.getId()).get(), null));
+                assertTrue(ticketType.getDeletedAt() instanceof LocalDateTime,
+                                "delete() method failed, deletedAt should be of type LocalDateTime");
+        }
 
-        TicketType ticketType = ticketTypeRepository
-                .save(new TicketType("adult", 29.99, null, eventRepository.findById(event.getId()).get(), null));
+        @Test
+        public void testTicketTypeRestore() {
+                ticketType.delete();
+                ticketType.restore();
 
-        ticketType.delete();
-        ticketType.restore();
+                assertEquals(ticketType.getDeletedAt(), null, "restore() method failed; deletedAt should be null");
+        }
 
-        assertEquals(ticketType.getDeletedAt(), null, "restore() method failed; deletedAt should be null");
+        @Test
+        public void testTicketTypeDTOCreation() {
+                TicketTypeDTO ticketTypeDTOControl = new TicketTypeDTO(ticketType.getId(), "adult", 29.99, null,
+                                ticketType.getEvent().getId());
+                TicketTypeDTO ticketTypeDTO = ticketType.toDTO();
 
-    }
-
-    @Test
-    public void testTicketTypeDTOCreation() {
-        // Set up
-        Zipcode zipcode = zipcodeRepository.save(new Zipcode("00100", "Helsinki"));
-        Venue venue = venueRepository.save(
-                new Venue("Bunkkeri", "Bunkkeritie 1", zipcodeRepository.findByZipcode(zipcode.getZipcode()), null));
-        Event event = eventRepository.save(new Event("Death metal karaoke", "Öriöriöriöriörirprir!!!!!", 10,
-                LocalDateTime.of(2055, 10, 12, 12, 00), LocalDateTime.of(2055, 10, 12, 12, 00),
-                LocalDateTime.of(2055, 10, 12, 12, 00), venueRepository.findById(venue.getId()).get(), null));
-
-        TicketType ticketType = ticketTypeRepository
-                .save(new TicketType("adult", 29.99, null, eventRepository.findById(event.getId()).get(), null));
-
-        TicketTypeDTO ticketTypeDTOControl = new TicketTypeDTO(ticketType.getId(), "adult", 29.99, null, ticketType.getEvent().getId());
-        TicketTypeDTO ticketTypeDTO = ticketType.toDTO();
-
-        assertEquals(ticketTypeDTOControl, ticketTypeDTO, "DTOs don't match");
-    }
+                assertEquals(ticketTypeDTOControl, ticketTypeDTO, "DTOs don't match");
+        }
 }
